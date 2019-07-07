1db0ae8f88addf07f4460ad3d4d7f75eb026f5d0
Valentin Nortier <52537670+Epidemicjack@users.noreply.github.com>
2019-07-07 18:29:00 +0200

    Add files via upload

---
+++ scriptmot.js
@@ -0,0 +1,29 @@
+Create input Array
+
+var mot1 = i0;
+var mot2 = i1;
+
+function checkForMissingInput(){
+    if(mot1.lenght =>1);
+    if (mot2.lenght =>1);
+
+}
+
+
+button onclick = function checker(){
+    
+    if(checkForMissingInput(false)){
+            alert('Fais un effort mon pote')
+    } else {
+        writeResult("output1")
+    }
+    
+}
+
+
+   button onclick = function WriteResult(){
+    // input 1 + input 2
+    document.getElementById("output1").value = mot1 + mot2 ;
+}
+
+
