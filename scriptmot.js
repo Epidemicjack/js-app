b3ff59d400600300d93dda301d66661a0e54f5bb
Valentin Nortier <52537670+Epidemicjack@users.noreply.github.com>
2019-07-09 17:13:05 +0200

    Add files via upload

--- scriptmot.js
+++ scriptmot.js
@@ -1,17 +1,24 @@
-Create input Array
+// bouton clickable
+var button = document.querySelector('button');
 
+//Récupère le mot n*1
+var mot1 = prompt('Premier truc')
 var mot1 = i0;
+//Récupère le mot n*2
+var mot2 = prompt('Deuxième truc')
 var mot2 = i1;
+// stock le résultat
+var result;
+
 
 function checkForMissingInput(){
-    if(mot1.lenght =>1);
-    if (mot2.lenght =>1);
+        // vérifie que les champs ont bien été remplis
 
 }
 
 
 button onclick = function checker(){
-    
+// Indique à l'utilisateur si les champs sont vides
     if(checkForMissingInput(false)){
             alert('Fais un effort mon pote')
     } else {
@@ -22,8 +29,9 @@ button onclick = function checker(){
 
 
    button onclick = function WriteResult(){
-    // input 1 + input 2
-    document.getElementById("output1").value = mot1 + mot2 ;
+    // écrit le résultat
+    console.log(i0+i1);
+    
 }
 
 
